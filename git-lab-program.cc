fa218ea6ec6b2d27af9cf0d2aedad6d129326462
Ethan <ee039221@ohio.edu>
2024-01-24 19:36:15 -0500

    Comitting C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ethan Eisnaugle
+ *        Date: Wednesday, January 24 2024
+ * Description: Lab 2 code
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
